9535d3d8957ab29db15e0eb0c15bdeb52524c9db
milypm <mp_morones@hotmail.com>
2020-12-09 18:58:28 -0600

    Update user interface to add TSE recommendation

--- bin/main.rb
+++ bin/main.rb
@@ -1,66 +1,80 @@
 # !/usr/bin/env ruby
 
-@position = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-def get_name(name)
-  puts "#{name} enter your name:"
-end
-
-def make_move(player)
-  puts "#{player} make your move"
-  puts "Select a position you want to move to from #{@position}"
-end
-
-def display_board
-  board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-  puts "             _________________
-                    |     |     |     |
-                    |  #{board[0][0]}  |  #{board[0][1]}  |  #{board[0][2]}  |
-                    |_____|_____|_____|
-                    |     |     |     |
-                    |  #{board[1][0]}  |  #{board[1][1]}  |  #{board[1][2]}  |
-                    |_____|_____|_____|
-                    |     |     |     |
-                    |  #{board[2][0]}  |  #{board[2][1]}  |  #{board[2][2]}  |
-                    |_____|_____|_____|"
-end
-
-def current_player(player)
-  make_move(player)
-  # move = gets.chomp.to_i
-  puts ''
-  puts "#{player} the move you made is invalid, please try again!"
-  display_board
-  puts ''
-  puts "Congs #{player}, you are the winner!"
-  puts ''
-  puts "It's a draw, you can play again!"
-  puts ''
-end
-
-print '                        TIC TAC TOE'
-display_board
-
 puts ''
-get_name('Player 1')
+print '                  TIC TAC TOE'
+puts '
+               _________________
+              |     |     |     |
+              |  1  |  2  |  3  |
+              |_____|_____|_____|
+              |     |     |     |
+              |  4  |  5  |  6  |
+              |_____|_____|_____|
+              |     |     |     |
+              |  7  |  8  |  9  |
+              |_____|_____|_____|
+'
+puts ''
+puts 'Player enter your name:'
 player_one = gets.chomp
 puts "#{player_one} your symbol is 'X'"
 puts ''
-get_name('Player 2')
+puts 'Player enter your name:'
 player_two = gets.chomp
 puts "#{player_two} your symbol is 'O'"
 puts ''
-# Game starts
-puts "                      ===============LET'S PLAY!==============="
-
+puts "            ===============LET'S PLAY!==============="
 game_ended = false
 is_player_one_move = true
-
 until game_ended
   if is_player_one_move
-    current_player(player_one)
+    # current_player(player_one)
+    puts "#{player_one} make your move"
+    puts ''
+    puts 'Select a position from [1, 2, 3, 4, 5, 6, 7, 8, 9]'
+    puts ''
+    puts "#{player_one} the move you made is invalid, please try again!"
+    puts '
+               _________________
+              |     |     |     |
+              |  1  |  2  |  3  |
+              |_____|_____|_____|
+              |     |     |     |
+              |  4  |  5  |  6  |
+              |_____|_____|_____|
+              |     |     |     |
+              |  7  |  8  |  9  |
+              |_____|_____|_____|
+'
+    puts ''
+    puts "Congs #{player_one}, you are the winner!"
+    puts ''
+    puts "It's a draw, you can play again!"
+    puts ''
     is_player_one_move = false
   else
-    current_player(player_two)
+    # current_player(player_two)
+    puts "#{player_two} make your move"
+    puts 'Select a position from [1, 2, 3, 4, 5, 6, 7, 8, 9]'
+    puts ''
+    puts "#{player_two} the move you made is invalid, please try again!"
+    puts '
+               _________________
+              |     |     |     |
+              |  1  |  2  |  3  |
+              |_____|_____|_____|
+              |     |     |     |
+              |  4  |  5  |  6  |
+              |_____|_____|_____|
+              |     |     |     |
+              |  7  |  8  |  9  |
+              |_____|_____|_____|
+'
+    puts ''
+    puts "Congs #{player_two}, you are the winner!"
+    puts ''
+    puts "It's a draw, you can play again!"
+    puts ''
     is_player_one_move = true
     game_ended = true
   end
