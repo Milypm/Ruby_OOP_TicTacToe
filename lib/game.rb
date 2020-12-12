2488d13eb6275d782884a11ffa66483c30153074
Galiwango Ananiya <galiwango101@gmail.com>
2020-12-13 00:40:12 +0300

    Fix trailing whitespace error

--- lib/game.rb
+++ lib/game.rb
@@ -56,13 +56,12 @@ class Game
 
   def row_one
     return true if @board[2][0] == @board[2][1] && @board[2][1] == @board[2][2]
-
   end
 
   def row_win?
     return true if @board[0][0] == @board[0][1] && @board[0][1] == @board[0][2]
     return true if @board[1][0] == @board[1][1] && @board[1][1] == @board[1][2]
-    
+
     row_one
     false
   end
