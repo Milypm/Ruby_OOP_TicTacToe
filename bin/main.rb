974e9c4a6af7fef8ae622e2e52bc70ea7f07ccf9
Galiwango Ananiya <galiwango101@gmail.com>
2020-12-12 12:43:46 +0300

    create prompt to for play on and do general code refactor

--- bin/main.rb
+++ bin/main.rb
@@ -2,9 +2,10 @@
 require_relative '../lib/game.rb'
 require_relative '../lib/player.rb'
 
-puts ''
-print '                  TIC TAC TOE'
 new_game = Game.new
+def gap
+  2.times { puts ''}
+end
 def valid_name?(name)
   return true unless name == ''
   false
@@ -22,9 +23,11 @@ def print_board(new_game)
     |     |     |     |
     |  #{new_game.board[2][0]}  |  #{new_game.board[2][1]}  |  #{new_game.board[2][2]}  |
     |_____|_____|_____|
-"
+  "
 end
 
+gap
+print '                  TIC TAC TOE'
 puts "
                _________________
               |     |     |     |
@@ -37,22 +40,20 @@ puts "
               |  #{new_game.board[2][0]}   |  #{new_game.board[2][1]}   |  #{new_game.board[2][2]}   |
               |_____|_____|_____|
 "
-puts ''
-
+gap
 while true
   print 'Player 1 enter your name: '
   player_one_name = gets.chomp
   if valid_name?(player_one_name)
     player_one = Player.new(player_one_name, 'X')
-    puts "#{player_one_name} your symbol is 'X'"
+    puts "#{player_one_name} your symbol is #{player_one_name}"
     break
   else
     puts 'Invalid name, try again'
-    puts ''
+    gap
     next
   end
 end
-
 while true
   puts ''
   print 'Player 2 enter your name: '
@@ -67,9 +68,9 @@ while true
     next
   end
 end
-puts ''
+gap
 puts "     ===============LET'S PLAY!==============="
-puts ''
+gap
 new_game.reset_board
 game_ended = false
 is_player_one_move = true
@@ -87,11 +88,12 @@ until game_ended
     print "#{player_two_name} select a position from #{positions}: "
     current_player = player_two_name
   end
+
   while true
     position = gets.chomp
     if new_game.is_choice_valid?(position)
-      spot = new_game.make_move(position)
-      if new_game.is_move_valid?(spot)
+      spot = new_game.get_spot(position)
+      if new_game.is_spot_valid?(spot)
         new_game.update_board(spot, symbol)
         positions.delete(position.to_i)
         break
@@ -101,11 +103,11 @@ until game_ended
     else
       puts 'Invalid position, try another one!'
     end
-    puts ''
+    gap
   end
+
   print_board(new_game)
-  puts ''
-  puts ''
+  gap
   if new_game.is_win? && symbol == 'X'
     puts "Congratz #{current_player}! You're the Winner :)"
     game_ended = true
@@ -120,4 +122,14 @@ until game_ended
       is_player_one_move = !is_player_one_move
     end
   end
+  gap
+#Prompt User to play on
+  if game_ended
+    print "Do you want to continue playing? , Enter 'Y' to continue or any key to exit :  "
+    response = gets.chomp.downcase
+    game_ended = response =='y' ? false : true
+    new_game.reset_board
+    positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
 end
--- lib/game.rb
+++ lib/game.rb
@@ -21,23 +21,36 @@ class Game
     false
   end
 
-  def make_move(choice)
-    position = {'1' => [0,0], '2' => [0,1], '3' => [0,2],
+  def get_spot(choice)
+    spot = {'1' => [0,0], '2' => [0,1], '3' => [0,2],
             '4' => [1,0], '5' => [1,1], '6' => [1,2],
             '7' => [2,0], '8' => [2,1], '9' => [2,2]
     }
-    position[choice]
+    spot[choice]
   end
  
-  def is_move_valid?(position)
-    return true if @board[position[0]][position[1]] != 'X' && @board[position[0]][position[1]] != 'O'
+  def is_spot_valid?(spot)
+    return true if @board[spot[0]][spot[1]] != 'X' && @board[spot[0]][spot[1]] != 'O'
     false
   end
   
-  def update_board(move, symbol)
-    @board[move[0]][move[1]] = symbol
+  def update_board(spot, symbol)
+    @board[spot[0]][spot[1]] = symbol
   end
 
+  def is_win?
+    return true if is_row_win?
+    return true if is_col_win?
+    return true if is_diag_win?
+    false
+  end
+
+  def is_draw?
+    return true if !is_win? && is_board_full?
+    false
+  end
+
+  private
   def is_row_win?
     return true if @board[0][0] == @board[0][1] && @board[0][1] == @board[0][2]
     return true if @board[1][0] == @board[1][1] && @board[1][1] == @board[1][2]
@@ -58,21 +71,9 @@ class Game
     false
   end
 
-  def is_win?
-    return true if is_row_win?
-    return true if is_col_win?
-    return true if is_diag_win?
-    false
-  end
-
   def is_board_full?
     return false if (@board.flatten).any?(Numeric)
     true
   end
-
-  def is_draw?
-    return true if !is_win? && is_board_full?
-    false
-  end
-
+  
 end
