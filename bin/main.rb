e9d7b0cbdd6c86dcace2ef82e4616ff86b3b1ea8
Galiwango Ananiya <galiwango101@gmail.com>
2020-12-12 23:10:30 +0300

    Refactor code in main.rb to reduce method line number

--- bin/main.rb
+++ bin/main.rb
@@ -4,8 +4,8 @@
 
 # rubocop:disable Metrics/AbcSize
 
-require_relative '../lib/game.rb'
-require_relative '../lib/player.rb'
+require_relative '../lib/game'
+require_relative '../lib/player'
 
 new_game = Game.new
 player_one = Player.new('', 'X')
@@ -13,34 +13,31 @@ player_two = Player.new('', 'O')
 positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 position = ''
 
-#Created the gap method to replace the repeated blank lines
 def gap
-  2.times { puts ''}
+  2.times { puts '' }
 end
 
 def valid_name?(name)
   return true unless name.empty?
+
   false
 end
 
 def print_board(new_game)
-  puts "
-     _________________
-    |     |     |     |
-    |  #{new_game.board[0][0]}  |  #{new_game.board[0][1]}  |  #{new_game.board[0][2]}  |
-    |_____|_____|_____|
-    |     |     |     |
-    |  #{new_game.board[1][0]}  |  #{new_game.board[1][1]}  |  #{new_game.board[1][2]}  |
-    |_____|_____|_____|
-    |     |     |     |
-    |  #{new_game.board[2][0]}  |  #{new_game.board[2][1]}  |  #{new_game.board[2][2]}  |
-    |_____|_____|_____|
-  "
+  puts "   _________________
+  |     |     |     |
+  |  #{new_game.board[0][0]}  |  #{new_game.board[0][1]}  |  #{new_game.board[0][2]}  |
+  |_____|_____|_____|
+  |     |     |     |
+  |  #{new_game.board[1][0]}  |  #{new_game.board[1][1]}  |  #{new_game.board[1][2]}  |
+  |_____|_____|_____|
+  |     |     |     |
+  |  #{new_game.board[2][0]}  |  #{new_game.board[2][1]}  |  #{new_game.board[2][2]}  |
+  |_____|_____|_____|"
 end
 
-#Created prompt method
 def prompt_user(new_player, code)
-  while true
+  loop do
     print "Player #{code} enter your name: "
     new_player.name = gets.chomp.strip
     if valid_name?(new_player.name)
@@ -48,7 +45,6 @@ def prompt_user(new_player, code)
       break
     else
       puts 'Invalid name, try again'
-      next
     end
   end
 end
@@ -68,7 +64,6 @@ puts "
               |_____|_____|_____|
 "
 gap
-#Created the prompt_user method and refactored out the two while loops
 prompt_user(player_one, 1)
 gap
 prompt_user(player_two, 2)
@@ -88,26 +83,24 @@ until game_ended
     print "#{player_two.name} select a position from #{positions}: "
     current_player = player_two.name
   end
-
-  while true
+  loop do
     position = gets.chomp
     if new_game.choice_valid?(position)
       spot = new_game.get_spot(position)
-      if new_game.spot_valid?(spot)
-        new_game.update_board(spot, symbol)
-        positions.delete(position.to_i)
-        break
-      else
-        puts 'That spot is already taken!, pick another one'
-      end
     else
-      puts 'Invalid position, try another one!'
+      print "#{current_player} Invalid position, try another one : "
+      next
     end
-    gap
+    if new_game.spot_valid?(spot)
+      new_game.update_board(spot, symbol)
+      positions.delete(position.to_i)
+      break
+    else
+      print "#{current_player} That spot is already taken!, pick another one : "
+    end
   end
-
   print_board(new_game)
-  
+  puts ''
   if new_game.win? && symbol == 'X'
     puts "Congratz #{current_player}! You're the Winner :)"
     gap
@@ -123,22 +116,21 @@ until game_ended
   else
     is_player_one_move = !is_player_one_move
   end
-#Prompt User to play on
+  next unless game_ended
+
+  print "Do you want to continue playing? , enter 'Y' to continue or any key to exit :  "
+  response = gets.chomp.downcase
+  game_ended = response != 'y'
+  new_game.reset_board
+  positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  gap
   if game_ended
-    print "Do you want to continue playing? , enter 'Y' to continue or any key to exit :  "
-    response = gets.chomp.downcase
-    game_ended = response =='y' ? false : true
-    new_game.reset_board
-    positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    puts 'Thanks for playing, goodbye!'
     gap
-    if game_ended
-      puts "Thanks for playing, goodbye!"
-      gap
-    else
-      puts "======== Welcome to your new game ========"
-      print_board(new_game)
-    end
+  else
+    puts '======== Welcome to your new game ========'
+    print_board(new_game)
   end
 end
 
-# rubocop:disable Metrics/AbcSize
+# rubocop:enable Metrics/AbcSize
