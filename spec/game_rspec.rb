eb4cb380dfe3623dd38f520caea69500d069634b
milypm <mp_morones@hotmail.com>
2020-12-22 18:29:27 -0600

    Add rspecs for initialize, reset_board, choice_valid? methods

--- spec/game_rspec.rb
+++ spec/game_rspec.rb
@@ -4,19 +4,28 @@ describe Game do
   
   describe '#initialize' do
     it 'create empty board (array)' do
-      expect(initialize.board).to eql(Array.new(3) { Array.new(3) })
+      game = Game.new
+      expect(game.board.instance_of? Array).to be_truthy
     end
   end
 
   describe '#reset_board' do
     it 'fill board (array) with nums (1-9)' do
-      expect(reset_board(rest_board)).to eql([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+      game = Game.new
+      game.reset_board
+      expect(game.board).to eql([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     end
   end
 
-  # describe '#choice_valid?' do
-  #   it 'fill board (array) with nums (1-9)' do
-  #     expect(choice_valid?(rest_board)).to eql([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-  #   end
-  # end
+  describe '#choice_valid?' do
+    it 'return true when passing a valid string' do
+      game = Game.new
+      expect(game.choice_valid?(1)).to be_falsy
+    end
+
+    it 'return true when passing a valid string' do
+      game = Game.new
+      expect(game.choice_valid?('1')).to be_truthy
+    end
+  end
 end
