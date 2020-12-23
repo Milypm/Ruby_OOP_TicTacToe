a3d86c543d927d21197e224a9b687c519544f868
milypm <mp_morones@hotmail.com>
2020-12-23 13:20:29 -0600

    Add rspecs for: get_spot, spot_valid?, update_board, win?, draw? methods in game_rspec file

---
+++ spec/player_rspec.rb
@@ -0,0 +1,20 @@
+require_relative '../lib/player'
+
+describe Player do
+  let(:name) { 'Nick' }
+  let(:player_1) { Player.new(name, 'X') }
+  let(:player_2) { Player.new(name, 'O') }
+  describe '#initialize' do
+    it 'set the name for player' do
+      expect(player_1.name).to eql('Nick')
+    end
+
+    it 'set the symbol for player one' do
+      expect(player_1.symbol).to eql('X')
+    end
+
+    it 'set the symbol for player two' do
+      expect(player_2.symbol).to eql('O')
+    end
+  end
+end
